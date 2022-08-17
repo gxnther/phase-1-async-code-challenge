d5ba35ecce58f2e8fb5ad2ea3d2778ecb9e16d97
Gunther <gunt.valentine@gmail.com>
2022-08-17 13:47:53 -0400

    Shows are now listed.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let qShows = document.querySelector("#queued");
-let shows = document.querySelector("list");
+let showList = document.querySelector("#list");
 let search = document.queryCommandIndeterm("#search")
+
+function createShows(show){
+    let listedShow = document.createElement(`li`)
+    listedShow.innerHTML = `${show.title}`
+    showList.append(listedShow)  
+}
 
 function getShows(){
     fetch(`http://localhost:3000/shows`)
     .then((resp) => resp.json())
-    .then((data) => {
-        console.log(data)
+    .then((showData) => showData.forEach(show => createShows(show)))
 }
+
+function start(){
+    getShows()
+}
+start()
