63f5ca19580a4aefd4a1ea2870d59a11d2c0f53c
Gunther <gunt.valentine@gmail.com>
2022-08-17 13:17:15 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code here
+let qShows = document.querySelector("#queued");
+let shows = document.querySelector("list");
+let search = document.queryCommandIndeterm("#search")
+
+function getShows(){
+    fetch(`http://localhost:3000/shows`)
+    .then((resp) => resp.json())
+    .then((data) => {
+        console.log(data)
+}
