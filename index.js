c98017053ae50edcd151780563122932b6364d62
Gunther <gunt.valentine@gmail.com>
2022-08-17 18:07:34 -0400

    Starting to filter

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
 let qShows = document.querySelector("#queued");
 let showList = document.querySelector("#list");
-let search = document.queryCommandIndeterm("#search")
+let form = document.querySelector("#search")
+let shows = []
+const filteredShows = []
 
-function createShows(show){
+
+
+function createShows(show) {
     let listedShow = document.createElement(`li`)
     listedShow.innerHTML = `${show.title}`
-    showList.append(listedShow)  
+    showList.append(listedShow)
     listedShow.addEventListener("click", (e) => {
         let newQueued = document.createElement(`li`)
         newQueued.innerHTML = `${show.title}`
         qShows.appendChild(newQueued)
-        newQueued.addEventListener("click" , (e) => {
-            console.log("asdas")
+        newQueued.addEventListener("click", (e) => {
             newQueued.remove()
         })
     })
 }
 
-
-function getShows(){
+function getShows() {
     fetch(`http://localhost:3000/shows`)
-    .then((resp) => resp.json())
-    .then((showData) => showData.forEach(show => createShows(show)))
+        .then((resp) => resp.json())
+        .then((showData) => {
+            console.log("show")
+            showData.forEach(show => {
+                shows.push(show.title)
+                createShows(show)
+            })
+        })
 }
 
-function start(){
+function start() {
+    document.querySelector(`form`).addEventListener(`submit`, (e) => {
+        e.preventDefault()
+        let query = e.target.search.value
+        console.log(query)
+        const filteredShows = shows.filter(show => {
+            return show.includes(query)
+        })
+        console.log(filteredShows)
+    })
     getShows()
 }
+
+
 
 start()
